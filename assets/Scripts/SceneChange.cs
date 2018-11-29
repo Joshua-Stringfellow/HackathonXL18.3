46d1db6b6152cc3b7b89a1179c32e5ec203a113a
Graham Porter <grahamporter03@gmail.com>
2018-11-29 11:28:31 -0600

    Added Project Files
    Added Project Files

---
+++ assets/Scripts/SceneChange.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.SceneManagement;
+
+public class SceneChange : MonoBehaviour
+{
+    public string Loading;
+
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.CompareTag("Boss"))
+        {
+            SceneManager.LoadScene(1);
+        }
+    }
+}
